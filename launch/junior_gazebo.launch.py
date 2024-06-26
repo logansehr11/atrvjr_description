b3040154f143a0862709ee9ba73bba26d9dd945d
logansehr11 <logansehr@gmail.com>
2024-06-26 10:35:10 -0400

    Minor Changes to Gazebo Launch File (Still Unable to Launch)

--- launch/junior_gazebo.launch.py
+++ launch/junior_gazebo.launch.py
@@ -1,30 +1,31 @@
 import os
+import xacro
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.substitutions import LaunchConfiguration
+
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, RegisterEventHandler
+from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
+
+from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import xacro
+
 
 def generate_launch_description():
 
-  use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-  urdf_file_name = 'urdf/experimental.urdf.xacro'
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    urdf_file_name = 'urdf/experimental.urdf.xacro'
 
-  print("urdf_file_name : {}".format(urdf_file_name))
+    print("urdf_file_name : {}".format(urdf_file_name))
 
 
-  robot_desc_cont = Command([
-    PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
-    PathJoinSubstitution([FindPackageShare('junior_description'), "urdf",
-            "experimental.urdf.xacro"])
-        ])
+    robot_desc_cont = Command([PathJoinSubstitution([FindExecutable(name='xacro')]), ' ', 
+                               PathJoinSubstitution([FindPackageShare('junior_description'), "urdf", "experimental.urdf.xacro"])])
 
-  robot_description = {"robot_description" : robot_desc_cont}
+    robot_description = {'robot_description' : robot_desc_cont}
 
-  return LaunchDescription([
+    return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
@@ -39,7 +40,7 @@ def generate_launch_description():
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time}, robot_description]
+            parameters=[{'use_sim_time': use_sim_time}, {'robot_description' : robot_description}]
             ),
 
         Node(
