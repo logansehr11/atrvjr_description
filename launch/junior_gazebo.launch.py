578c695a2637d962b6ff19d893434eedbf67d975
logansehr11 <logansehr@gmail.com>
2024-07-18 11:03:38 -0400

    Made Gazebo Supported Rviz Config

---
+++ launch/junior_gazebo.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    packagePath = FindPackageShare('atrvjr_description')
+
+    
+
+    ld.add_action(
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{
+                'robot_description': ParameterValue(
+                    Command([
+                        'xacro ',
+                        PathJoinSubstitution([packagePath,'urdf', 'junior.urdf.xacro'])
+                    ]), 
+                    value_type=str
+                ),
+            }]
+        )
+    )
+
+    ld.add_action(Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', PathJoinSubstitution([packagePath, 'config', 'junior_gazebo.rviz'])],
+    ))
+
+    return ld
